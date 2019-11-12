e087fb3930d2c976dd32709853b000c0a40f027b
Axel Espinosa <axeldavid45@gmail.com>
2019-11-12 13:18:49 -0600

    Terminamos funcion RObjetivo

--- twophases/Objective.java
+++ twophases/Objective.java
@@ -3,6 +3,7 @@ package twophases;
 public abstract class  Objective {
     double[] coeficients;
     int typeOptimization; // Max = 1, Min = 2
+    boolean Stoppable;
     
     abstract void toStandard();
 }
--- twophases/RObjective.java
+++ twophases/RObjective.java
@@ -1,11 +1,36 @@
-
 package twophases;
 
 public class RObjective extends Objective {
+    
+    public RObjective(int nArtificials) {
+        //Creamos el arreglo con los coeficientes negativos
+        this.coeficients = new double[nArtificials];
+        for(int i = 0; i < nArtificials - 1; i++)
+            coeficients[i] = -1; //Llenamos el arreglo con los coeficientes negativos
+        coeficients[nArtificials] = 0; //Colocamos el resultado de la funcion R en 0
+        this.Stoppable = false; //Colocamos que no es detenible al inicio
+    }
+    
+    //Metodo que actualiza la funcion R
+    protected void updateIndex(double value, int index) {
+        this.coeficients[index] = value;
+    }
+    
+    // Determina cuando la funcion objetivo vuelve a ser -1
+    private boolean isStoppable() {
+        //Recorremos los coeficientes para saber si son diferentes a -1 y seguir con el algoritmo
+        for(int i = 0; i < (this.coeficients.length - 1); i++)
+           if(coeficients[i] != -1)
+               this.Stoppable = false;
+        return this.Stoppable;
+    }
+    
+    //Si los coeficientes no son iguales a -1 y la solucion no es igual a 0  no tiene solucion
+    private boolean hasInitialSolution() {
+        return this.coeficients[this.coeficients.length] == 0 && this.isStoppable();
+    }
 
     @Override
-    void toStandard() {
-        
-    }
+    void toStandard() { }
     
 }
