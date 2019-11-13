6f897d2b2022c5447efcfe04d84888c0d0c8a6bc
Axel Espinosa <axeldavid45@gmail.com>
2019-11-13 17:36:47 -0600

    Creacion de Table y constraints

--- twophases/Constraints.java
+++ twophases/Constraints.java
@@ -1,6 +1,29 @@
 package twophases;
 
-
+ 
 public class Constraints {
+    double[] Coeficients;
+    String Operator;
+    boolean vArtificial, vSlack;
+    
+    private void toStandard() {
+        
+    }
 
+    public boolean hasVArtificial() {
+        return vArtificial;
+    }
+
+    public void setvArtificial(boolean vArtificial) {
+        this.vArtificial = vArtificial;
+    }
+
+    public boolean hasVSlack() {
+        return vSlack;
+    }
+
+    public void setvSlack(boolean vSlack) {
+        this.vSlack = vSlack;
+    }
+    
 }
--- twophases/Table.java
+++ twophases/Table.java
@@ -2,5 +2,9 @@ package twophases;
 
 
 public class Table {
-
+    double[][] Matrix; //Contiene los datos para hacer iteraciones
+    double[] Solutions; //Contiene las soluciones de cada fila
+    double[][] Constraints; //Contiene las restricciones
+    Objective FObjective; //Contiene la funcion objetivo
+    int enteringColumn, leavingRow; //Contiene la posicion de la columna que entra y la fila que sale
 }
