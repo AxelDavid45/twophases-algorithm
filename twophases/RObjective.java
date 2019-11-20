7e20368c4e47db1302d36d22d42d957fd1b46159
Axel Espinosa <axeldavid45@gmail.com>
2019-11-20 09:52:21 -0600

    Agregado metodo para poder imprimir

--- twophases/RObjective.java
+++ twophases/RObjective.java
@@ -1,34 +1,43 @@
 package twophases;
 import java.util.ArrayList;
 public class RObjective extends Objective {
-    
+    public double Solution;
     public RObjective(int nArtificials) {
         //Creamos el arreglo con los coeficientes negativos
         this.coeficients = new ArrayList<>();
-        for(int i = 0; i < nArtificials - 1; i++)
+        //Agregamos valores -1 dependiento del numero de nArtificiales
+        for(int i = 0; i < nArtificials; i++)
             this.coeficients.add((double) -1); //Llenamos el arreglo con los coeficientes negativos
-        this.coeficients.add((double) 0); //Colocamos el resultado de la funcion R en 0
-        this.Stoppable = false; //Colocamos que no es detenible al inicio
+        this.Solution = 0; //Colocamos el resultado de la funcion R en 0
+        this.Stoppable = true; //Colocamos que no es detenible al inicio
+    }
+    
+    public double getSolution() {
+        return this.Solution;
     }
     
     //Metodo que actualiza la funcion R
     @Override
     protected void updateIndex(double value, int index) {
-        this.coeficients.add(index, value);
+        this.coeficients.set(index, value);
     }
     
-    // Determina cuando la funcion objetivo vuelve a ser -1
-    private boolean isStoppable() {
+    /*
+        Determina cuando la funcion objetivo vuelve a ser -1, si cualquiera de los elementos es diferente a -1 
+        no puede ser detenible
+    */
+    public boolean isStoppable() {
         //Recorremos los coeficientes para saber si son diferentes a -1 y seguir con el algoritmo
-        for(int i = 0; i < (this.coeficients.length - 1); i++)
-           if(coeficients[i] != -1)
+        for(int i = 0; i < (this.coeficients.size()-1); i++)
+           if(coeficients.get(i) != -1)
                this.Stoppable = false;
         return this.Stoppable;
     }
     
     //Si los coeficientes no son iguales a -1 y la solucion no es igual a 0  no tiene solucion
-    private boolean hasInitialSolution() {
-        return this.coeficients[this.coeficients.length] == 0 && this.isStoppable();
+    public boolean hasInitialSolution() {
+        return this.coeficients.get(this.coeficients.size()-1) == 0 && this.isStoppable();
+          
     }
 
     @Override
--- twophases/Table.java
+++ twophases/Table.java
@@ -3,6 +3,7 @@ package twophases;
 import java.util.Vector;
 
 public class Table {
+
     double[][] Matrix; //Contiene los datos para hacer iteraciones
     Vector Solutions; //Contiene las soluciones de cada fila el ultimo elemento es la sol de R o Z
     Constraint[] Constraints; //Contiene las restricciones
@@ -10,19 +11,20 @@ public class Table {
     Objective ZObjective = null, RObjective = null; //Contiene la funcion objetivo o la R
     int enteringColumn, leavingRow; //Contiene la posicion de la columna que entra y la fila que sale
     int nRows, nColumns; //Numero de filas y columnas que tendra la matriz
-    
+
     public Table(Constraint[] constraints, Objective fObjective) {
         //Inicializamos el arreglo solutions de tamano constraints + 1 donde 1 es la Objective Z o R
         this.Solutions = new Vector(constraints.length + 1);
         //Recorremos el array y contabilizamos cuantas varibles de holgura o artificales tenemos
-        for(Constraint x: constraints) {
+        for (Constraint x : constraints) {
             if (x.hasVArtificial() && x.hasVSlack()) {
                 this.nArtificial++;
                 this.nSlack++;
-            } else if (x.hasVSlack()) 
+            } else if (x.hasVSlack()) {
                 this.nSlack++;
-            else if (x.hasVArtificial())
+            } else if (x.hasVArtificial()) {
                 this.nArtificial++;
+            }
             //Guardamos las soluciones de cada restriccion en el arreglo Solutions
             this.Solutions.addElement(x.getSolution());
         }
@@ -33,12 +35,12 @@ public class Table {
         //Asignamos la funcion objetivo Z
         this.ZObjective = fObjective;
     }
-    
+
     /*
         Build matrix nos servira para poder construir la matriz y despues a partir de los
         metodos phase1 y doSimplex modificarla para hacer el algoritmo dependiendo de si tiene 
         variables artificiales o no
-    */
+     */
     public void buildMatrix() {
         //Asignamos el tamano de nuestra matriz usando la siguiente regla
         //Las columnas seran de acuerdo al numero de coeficientes que tenga la funcion Z + vArtifiales + VSlack + 1, 1 que sera la columna de soluciones
@@ -47,54 +49,64 @@ public class Table {
         this.setnColumns(this.ZObjective.numberCoeficients() + this.nArtificial + this.nSlack + 1);
         this.Matrix = new double[this.getnRows()][this.getnColumns()];
         //Como de principio Objective no cuenta con valores en las columnas de vSlack o VArtificial agregamos valor 0
-        for (int i = 0; i < 1; i++)
-            for (int j = this.ZObjective.numberCoeficients(); j < nColumns; j++)
+        for (int i = 0; i < 1; i++) {
+            for (int j = this.ZObjective.numberCoeficients(); j < nColumns; j++) {
                 this.ZObjective.coeficients.add((double) 0);
-            
+            }
+        }
+
         //Introducimos la fObjective Z por default
         for (int i = 0; i < 1; i++) {
             for (int j = 0; j < this.getnColumns(); j++) {
-                this.Matrix[i][j] = this.ZObjective.coeficients.get(j); 
+                this.Matrix[i][j] = this.ZObjective.coeficients.get(j);
             }
         }
         //Variable auxiliar que contiene el # de coeficientes de la restriccion
         int counterCoeficients = this.Constraints[0].getNumberCoeficients();
         int indexConstraint = 0, IndexConstraints = 0;
-        //Llenamos la matriz con los datos que ya tenemos comenzando una fila
+        //Llenamos la matriz con los datos que ya tenemos comenzando una fila abajo
         for (int i = 1; i <= this.getnRows() - 1; i++) {
             for (int j = 0; j < this.getnColumns(); j++) {
-                if (indexConstraint < counterCoeficients)
-                    this.Matrix[i][j] = this.Constraints[IndexConstraints].Coeficients[indexConstraint];    
+                //Comprueba que no rebasemos el limite de elementos de las constraints
+                if (indexConstraint < counterCoeficients) {
+                    this.Matrix[i][j] = this.Constraints[IndexConstraints].Coeficients[indexConstraint];
+                } else {
+                    //Comprobamos si hemos alcanzado la col de soluciones e insertamos la sol de cada constraint
+                    this.Matrix[i][this.getnColumns()-1] = this.Constraints[IndexConstraints].getSolution();
+                }
                 indexConstraint++; //Aumentamos el indice para ir cambiando de coeficiente
             }
             indexConstraint = 0; //Reseteamos el indice para la siguiente restriccion
             IndexConstraints++; //Aumentamos la posicion del indice para recorrer el array restricciones
         }
-        
+
     }
     
+    /*
+        Metodo que remplaza la funcion objecito z o r dependiendo
+    */
+    public void replaceRObjective(Objective x) {
+        
+    }
+
     public void doSimplex(boolean type) {
-        
+
     }
-    
+
     public void phase1() {
-        
+
     }
-    
-    public void replaceObjective(Objective x) {
-        
-    }
-    
+
     public void updateTable() {
-        
+
     }
-    
+
     public void isTie() {
-        
+
     }
-    
+
     public void isThereSolution() {
-        
+
     }
 
     public Vector getSolutions() {
@@ -116,8 +128,9 @@ public class Table {
     public int getLeavingRow() {
         return leavingRow;
     }
-   
-    public void showResult() {}
+
+    public void showResult() {
+    }
 
     public int getnRows() {
         return nRows;
@@ -134,5 +147,5 @@ public class Table {
     public void setnColumns(int nColumns) {
         this.nColumns = nColumns;
     }
-    
+
 }
